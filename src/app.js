a15df1202d47c45474ef9ce8a79a44d53a1b7de6
Marcelo Andrés Cornejo Maldonado <73359763+elmarceloc@users.noreply.github.com>
2025-02-06 05:02:55 +0000

    dirname

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import chatRouter from "./routes/chatRouter.js";
 
 const app = express();
+import path from 'path'
 const __dirname = import.meta.dirname;
 
 // parse json request body
@@ -15,7 +16,7 @@ app.use(cors());
 app.use(express.static("public"));
 
 app.get('/', function(req, res) {
-  res.sendFile(__dirname+'/public/index.html');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.use("/api", chatRouter);
