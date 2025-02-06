7f493bd9c64eba892aa434dbdd85e3b35db34497
Marcelo Andrés Cornejo Maldonado <73359763+elmarceloc@users.noreply.github.com>
2025-02-06 05:06:46 +0000

    fix

--- src/app.js
+++ src/app.js
@@ -1,19 +1,20 @@
 import express from "express";
 import cors from "cors";
-
+import path from "path";
 import chatRouter from "./routes/chatRouter.js";
 
+// Solución para obtener __dirname en módulos ESM
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
-import path from 'path'
-const __dirname = import.meta.dirname;
 
 // parse json request body
 app.use(express.json());
 
 // enable cors
-app.use(cors());
-
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
