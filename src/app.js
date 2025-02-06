d1f1351801a2ea499d4a4e16d5fc9dc804baee03
Marcelo Andrés Cornejo Maldonado <73359763+elmarceloc@users.noreply.github.com>
2025-02-06 04:59:32 +0000

    html

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(express.static("public"));
 
 app.get('/', function(req, res) {
-  res.sendFile(path.__dirname + '/public/index.html');
+  res.sendFile('/public/index.html');
 });
 
 app.use("/api", chatRouter);
